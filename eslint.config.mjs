09ec8cbc728a1d1f2b8fb8f6a557dcd6644b2a0b
cheeseburgertony <1821955020@qq.com>
2025-09-17 00:48:02 +0800

    chore: 调整依赖版本&进行ESLint配置

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json", // 添加这行启用类型检查
+            },
+            globals: { ...globals.browser, ...globals.node }
+        },
+        plugins: {
+            "@typescript-eslint": tsPlugin
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            ...tsPlugin.configs.recommended.rules,
+						"@typescript-eslint/no-unnecessary-try-catch": "off",
+            "@typescript-eslint/no-explicit-any": "error",
+            "@typescript-eslint/no-unused-vars": "error",
+            "no-unused-expressions": "off",
+            "no-undef": "off",
+            "no-constant-condition": "off"
+        }
+    }
+];

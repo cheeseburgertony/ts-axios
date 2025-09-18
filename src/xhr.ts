22cbdf686c7f10ea4fcfce43e48989d7dd2b413e
cheeseburgertony <1821955020@qq.com>
2025-09-18 13:07:25 +0800

    feat: 添加错误处理和对应示例

--- src/xhr.ts
+++ src/xhr.ts
@@ -2,12 +2,25 @@ import { parseHeaders } from "./helper/headers";
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "./types";
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-	return new Promise((resolve) => {
-		const { data = null, url, method = "get", headers, responseType } = config;
+	return new Promise((resolve, reject) => {
+		const {
+			data = null,
+			url,
+			method = "get",
+			headers,
+			responseType,
+			timeout
+		} = config;
 		const request = new XMLHttpRequest();
+
 		if (responseType) {
 			request.responseType = responseType;
 		}
+
+		if (timeout) {
+			request.timeout = timeout;
+		}
+
 		request.open(method.toUpperCase(), url, true);
 
 		request.onreadystatechange = function handleLoad() {
@@ -28,7 +41,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 				config,
 				request
 			};
-			resolve(response);
+			handleResponse(response);
+		};
+
+		request.onerror = function handleError() {
+			reject(new Error("Network Error"));
+		};
+
+		request.ontimeout = function handleTimeout() {
+			reject(new Error(`Timeout of ${timeout} ms exceeded`));
 		};
 
 		Object.keys(headers).forEach((name) => {
@@ -40,5 +61,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 		});
 
 		request.send(data);
+
+		function handleResponse(response: AxiosResponse) {
+			if (response.status >= 200 && response.status < 300) {
+				resolve(response);
+			} else {
+				reject(new Error(`Request failed with status code ${response.status}`));
+			}
+		}
 	});
 }
